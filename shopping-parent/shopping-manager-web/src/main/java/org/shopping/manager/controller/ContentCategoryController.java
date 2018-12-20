25d8eb3df409d0866ff4c9d959dee7c903455073
CKJ <qaz1780526188@163.com>
2018-12-21 00:28:21 +0800

    广告功能完成

---
+++ shopping-parent/shopping-manager-web/src/main/java/org/shopping/manager/controller/ContentCategoryController.java
@@ -0,0 +1,125 @@
+package org.shopping.manager.controller;
+
+/**
+ * 广告类目
+ */
+import java.util.List;
+
+import org.content.service.ContentCategoryService;
+import org.shopping.common.Enumeration;
+import org.shopping.pojo.TbContentCategory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+
+import entity.PageResult;
+import entity.Result;
+
+@RestController
+@RequestMapping("/ContentCategory")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ContentCategoryController {
+
+	@Reference
+	private ContentCategoryService Service;
+
+	/**
+	 * 获取全部信息
+	 * 
+	 * @return
+	 */
+
+	@RequestMapping("/findAll")
+	public List<TbContentCategory> findAll() {
+		return Service.queryAll();
+	}
+
+	/**
+	 * 返回分页列表
+	 * 
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult findPage(int page, int rows) {
+		return Service.queryPageListByWhere(new TbContentCategory(), page, rows);
+	}
+
+	/**
+	 * 增加
+	 * 
+	 * @param bean
+	 * @return
+	 */
+
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbContentCategory bean) {
+		try {
+			Service.save(bean);
+			return new Result(Enumeration.CODE_SUCCESS, true, Enumeration.INSETR_SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(Enumeration.CODE_SUCCESS, false, Enumeration.INSETR_FAIL);
+		}
+	}
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param ids
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long[] ids) {
+		if (Service.delete(ids)) {
+			return new Result(Enumeration.CODE_SUCCESS, true, Enumeration.DELETE_SUCCESS);
+		}
+		return new Result(Enumeration.CODE_SUCCESS, false, Enumeration.DELETE_FAIL);
+
+	}
+
+	/**
+	 * 修改对象
+	 * 
+	 * @param ContentCategory
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbContentCategory bean) {
+		try {
+			Service.update(bean);
+			return new Result(Enumeration.CODE_SUCCESS, true, Enumeration.UPDATA_SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(Enumeration.CODE_SUCCESS, false, Enumeration.UPDATA_FAIL);
+		}
+	}
+
+	/**
+	 * 根据id获取对象
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbContentCategory findOne(Long id) {
+		return Service.queryById(id);
+	}
+
+	/**
+	 * 查询+分页
+	 * 
+	 * @param ContentCategory
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(int page, int rows, @RequestBody TbContentCategory bean) {
+		return Service.findPage(bean, page, rows);
+	}
+
+}
