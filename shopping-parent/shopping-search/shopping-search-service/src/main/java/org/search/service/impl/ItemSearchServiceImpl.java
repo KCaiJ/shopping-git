3591f6893919481a8670f7e3338681ac6a57ae49
CKJ <qaz1780526188@163.com>
2018-12-24 20:56:14 +0800

    Update ItemSearchServiceImpl.java

--- shopping-parent/shopping-search/shopping-search-service/src/main/java/org/search/service/impl/ItemSearchServiceImpl.java
+++ shopping-parent/shopping-search/shopping-search-service/src/main/java/org/search/service/impl/ItemSearchServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.data.solr.core.query.Criteria;
 import org.springframework.data.solr.core.query.Query;
 import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.data.solr.core.query.result.ScoredPage;
-
 import com.alibaba.dubbo.config.annotation.Service;
 
 @Service(timeout=3000)
@@ -22,7 +21,7 @@ public class ItemSearchServiceImpl implements ItemSearchService{
 	@Override
 	public Map<String, Object> search(Map searchMap) {
 		Map<String,Object> map=new HashMap<>();
-		Query query=new SimpleQuery();
+		Query query=new SimpleQuery("*:*");
 		//添加查询条件
 		Criteria criteria=new Criteria("item_keywords").is(searchMap.get("keywords"));
 		query.addCriteria(criteria);
